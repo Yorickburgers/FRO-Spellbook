4d8d39bbaa54761ba6c8fe6771cabfa3fef9e62b
yorickburgers <yorickburgers@live.nl>
2025-01-12 15:49:16 +0100

    base structure nav-bar

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,10 +1,36 @@
 import './Navigation.css';
+import logo from "../../../public/vite.svg"
 
 function Navigation({}) {
 return (
-<>
-
-</>
+    <nav>
+        <div className="outer-nav-container">
+            <span className="logo-wrapper">
+                <a href="/">
+                    <img src={logo} alt="spellbook icon"/>
+                    <h1 className="logoName">Spellbook</h1>
+                </a>
+            </span>
+        </div>
+        <div className="outer-nav-container">
+            <p>Home</p>
+            <p>All spells</p>
+            <p>Favourites</p>
+            <input placeholder='for example "wish"' type="text" name="searchTerm"/>
+            <button
+                type="button"
+            >zoek </button>
+        </div>
+        <div className="outer-nav-container">
+            <div className="input-container">
+                <input type="text" name="userName" placeholder="Username"/>
+                <input type="text" name="password" placeholder="Password"/>
+            </div>
+            <button
+                type="text"
+            >login</button>
+        </div>
+    </nav>
 );
 }
 
