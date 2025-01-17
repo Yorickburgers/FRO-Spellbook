d4222f560d8c246c57f1e05d699f51f2b3587613
yorickburgers <yorickburgers@live.nl>
2025-01-17 17:09:46 +0100

    made SortButton component for catalogue

---
+++ src/components/sortButton/SortButton.jsx
@@ -0,0 +1,11 @@
+import './SortButton.css';
+
+function SortButton({text, sortStyle}) {
+    return (
+        <button type="button" className="sorting-button" onClick={sortStyle}>
+            {text}
+        </button>
+    );
+}
+
+export default SortButton;
