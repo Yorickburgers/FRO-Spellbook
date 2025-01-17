70990899b7aa739520862bc9fc3363ca96432340
yorickburgers <yorickburgers@live.nl>
2025-01-17 17:21:25 +0100

    replaced flat text with SortButton components on catalogue page, added some styling

--- src/pages/catalogue/Catalogue.jsx
+++ src/pages/catalogue/Catalogue.jsx
@@ -1,4 +1,5 @@
 import './Catalogue.css';
+import SortButton from "../../components/sortButton/SortButton.jsx";
 
 function Catalogue() {
 return (
@@ -7,13 +8,31 @@ return (
         <div className="inner-container filters">test</div>
         <div className="inner-container">
             <div className="sorting-tags-container">
-                <h2>Name</h2>
-                <h2>Class</h2>
-                <h2>Damage</h2>
-                <h2>Type</h2>
-                <h2>Duration</h2>
-                <h2>Range</h2>
-            </div>
+                <SortButton
+                    text="Name"
+                    sortStyle="null"
+                />
+                <SortButton
+                    text="Class"
+                    sortStyle="null"
+                />
+                <SortButton
+                    text="Damage"
+                    sortStyle="null"
+                />
+                <SortButton
+                    text="Type"
+                    sortStyle="null"
+                />
+                <SortButton
+                    text="Duration"
+                    sortStyle="null"
+                />
+                <SortButton
+                    text="Range"
+                    sortStyle="null"
+                />
+                </div>
             <ul>
                 <li>spell 1</li>
                 <li>spell2</li>
