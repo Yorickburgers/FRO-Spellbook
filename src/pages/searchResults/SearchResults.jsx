86756ce577c01f0fa3dcb1a1f98c98c82bff7d41
yorickburgers <yorickburgers@live.nl>
2025-01-23 20:38:24 +0100

    added filter function that takes the searchterm from searchbars as parameter

--- src/pages/searchResults/SearchResults.jsx
+++ src/pages/searchResults/SearchResults.jsx
@@ -1,12 +1,13 @@
 import './SearchResults.css';
 import {useEffect, useState} from "react";
 import axios from "axios";
-import {useNavigate} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 
 function SearchResults() {
     const [loading, setLoading] = useState(true);
-    const [allSpells, setAllSpells] = useState({});
-
+    const [allSpells, setAllSpells] = useState([]);
+    const [filteredSpells, setFilteredSpells] = useState([]);
+    const {searchTerm} = useParams();
 
     useEffect(() => {
         const controller = new AbortController();
@@ -16,7 +17,7 @@ function SearchResults() {
                 const response = await axios.get(`https://www.dnd5eapi.co/api/spells`, {
                     signal: controller.signal,
                 });
-                setAllSpells(response.data);
+                setAllSpells(response.data.results);
             } catch (e) {
                 console.error(e);
             } finally {
@@ -32,27 +33,11 @@ function SearchResults() {
     }, []);
 
     useEffect(() => {
-        const controller = new AbortController();
-
-        async function getSpells() {
-            try {
-                const response = await axios.get(`https://www.dnd5eapi.co/api/spells`, {
-                    signal: controller.signal,
-                });
-                setAllSpells(response.data);
-            } catch (e) {
-                console.error(e);
-            } finally {
-                setLoading(false);
-            }
-        }
-
-        getSpells();
-
-        return function cleanup() {
-            controller.abort();
-        }
-    }, [allSpells]);
+        const filteredSpells = allSpells.filter((spell) => {
+            return !!spell.name.includes(`${searchTerm}`);
+        });
+        setFilteredSpells(filteredSpells);
+    }, [allSpells, searchTerm]);
 
 
     return (
