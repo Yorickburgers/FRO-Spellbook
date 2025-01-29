1ba0732071f5a83de5ae2157bf5e05173e2527e6
yorickburgers <yorickburgers@live.nl>
2025-01-29 17:24:06 +0100

    made inputs controlled

--- src/pages/account/Account.jsx
+++ src/pages/account/Account.jsx
@@ -1,38 +1,80 @@
 import './Account.css';
+import {useEffect, useState} from "react";
 
 function Account() {
-return (
-<main className="page-container">
-    <h1 className="page-title">Account</h1>
-    <div className="account-outer-container">
-        <section className="account-inner-container">
-            <h2>Login</h2>
-            <label className="account-label" htmlFor="login-username">Username:
-                <input type="text" className="accountInput"/>
-            </label>
-            <label className="account-label" htmlFor="login-password">Password:
-                <input type="text" className="accountInput"/>
-            </label>
-            <h3>Don't have an account? Register a new one! </h3>
-            <button type="button" className="button"><h2>Login</h2></button>
-        </section>
-        <div className="separation"></div>
-        <section className="account-inner-container">
-            <h2>Register</h2>
-            <label className="account-label" htmlFor="register-username">Username
-                <input type="text" id="register-username" className="accountInput"/>
-            </label>
-            <label className="account-label" htmlFor="register-password">Password
-                <input type="text" id="register-password" className="accountInput"/>
-            </label>
-            <label className="account-label" htmlFor="confirm">Confirm password
-                <input type="text" id="confirm" className="accountInput"/>
-            </label>
-            <button type="button" className="button"><h2>Register</h2></button>
-        </section>
-    </div>
-</main>
-);
+    const [loginInput, setLoginInput] = useState({
+        username: "",
+        password: "",
+    });
+    const [registerInput, setRegisterInput] = useState({
+        username: "",
+        password: "",
+        confirm: "",
+    })
+    const [error, setError] = useState("");
+
+    function handleLoginChange(e) {
+        setLoginInput(e.target.value);
+    }
+
+    function handleRegisterChange(e) {
+        const {name, value} = e.target;
+        setRegisterInput((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    }
+
+    useEffect(() => {
+        registerInput.password !== registerInput.confirm && setError( "passwords don't match, try again")
+    }, [registerInput]);
+
+    function logInput() {
+        console.log(registerInput);
+    }
+
+    return (
+        <main className="page-container">
+            <h1 className="page-title">Account</h1>
+            <div className="account-outer-container">
+                <section className="account-inner-container">
+                    <h2 className="section-title">Login</h2>
+                    <form className="account-form">
+                        <label className="account-label" htmlFor="login-username">Username:
+                            <input type="text" className="accountInput" id="login-username" name="username" onChange={handleLoginChange}/>
+                        </label>
+                        <label className="account-label" htmlFor="login-password">Password:
+                            <input type="password" className="accountInput" id="login-password" name="password" onChange={handleLoginChange}/>
+                        </label>
+                        <h3 className="register-message">Don't have an account? Register a new one! </h3>
+                        <button type="submit" className="button">
+                            <h2>Login</h2>
+                        </button>
+                    </form>
+                    <div className="account-error"></div>
+                </section>
+                <div className="separation"></div>
+                <section className="account-inner-container">
+                    <h2 className="section-title">Register</h2>
+                    <form className="account-form">
+                        <label className="account-label" htmlFor="register-username">Username:
+                            <input type="text" id="register-username" name="username" className="accountInput" onChange={handleRegisterChange} value={registerInput.username}/>
+                        </label>
+                        <label className="account-label" htmlFor="register-password">Password:
+                            <input type="password" id="register-password" name="password" className="accountInput" onChange={handleRegisterChange} value={registerInput.password}/>
+                        </label>
+                        <label className="account-label" htmlFor="confirm">Confirm password:
+                            <input type="password" id="confirm" name="confirm" className="accountInput" onChange={handleRegisterChange} value={registerInput.confirm}/>
+                        </label>
+                        <button type="submit" className="button" onClick={logInput}>
+                            <h2>Register</h2>
+                        </button>
+                    </form>
+                    <p className="account-error">{error}</p>
+                </section>
+            </div>
+        </main>
+    );
 }
 
 export default Account;
